2219a168c41d44861058a83a1d3f9e39ecccf969
Laurent Brucher <laurent.brucher@mckinsey.com>
2024-04-16 09:12:25 +0200

    Initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { ClientsComponent } from './clients/clients.component';
+
+export const routes: Routes = [
+    {path: '',        redirectTo: '/clients', pathMatch: 'full'},
+    {path: 'clients', component: ClientsComponent},
+    {path: '**',      redirectTo: '/'},
+];
