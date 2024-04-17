cf9fd7c1a20da39e7b04217b3ea834bc69f5744d
Laurent Brucher <laurent.brucher@mckinsey.com>
2024-04-17 18:26:44 +0200

    Fix test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, POC-FE-LaurentB');
+    expect(compiled.querySelector('.navbar-brand')?.textContent).toContain('The Best Banking Company');
   });
 });
