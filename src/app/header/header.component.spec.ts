155557a46b1cfcdb7d86f3d21c5c75d87651fbf8
Laurent Brucher <laurent.brucher@mckinsey.com>
2024-04-18 16:25:44 +0200

    Disable header tests (no tests for now)

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -2,20 +2,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
 
-describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
+// describe('HeaderComponent', () => {
+//   let component: HeaderComponent;
+//   let fixture: ComponentFixture<HeaderComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [HeaderComponent]
-    })
-    .compileComponents();
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       imports: [HeaderComponent]
+//     })
+//     .compileComponents();
     
-    fixture = TestBed.createComponent(HeaderComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+//     fixture = TestBed.createComponent(HeaderComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
 
-  //TODO...
-});
+//   //TODO...
+// });
